64d1c186c0ae319a72807158a7c2b9241e1e4401
azat-hafizov <azat@optio.finance>
2023-10-30 02:47:22 +0500

    test(base): update default tests

---
+++ test/TestExample.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {AppContract} from "../src/App.sol";
+import {console} from "forge-std/console.sol";
+
+contract TestExample is TestBase {}
